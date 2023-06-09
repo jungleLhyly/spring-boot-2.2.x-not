041e00d54ec34ff4ce6fee93d9c8b641b017ae12
jungle <liyan@lhyly.com>
2023-06-09 12:57:50 +0800

    init

---
+++ jungle-test/src/main/java/com/jungle/spring/Program.java
@@ -0,0 +1,14 @@
+package com.jungle.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liyan at 2023/6/9
+ */
+@SpringBootApplication
+public class Program {
+	public static void main(final String[] args) {
+		SpringApplication.run(Program.class, args);
+	}
+}
